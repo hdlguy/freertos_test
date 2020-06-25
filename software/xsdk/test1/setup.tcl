a7d882aed62633a7db35ba638366571a41ddb548
pedro <hdlguy@hotmail.com>
2020-06-25 18:33:07 -0400

    tcl cleanup

--- software/xsdk/test1/setup.tcl
+++ software/xsdk/test1/setup.tcl
@@ -3,9 +3,7 @@ set sdk_dir ./dma_test.sdk
 
 file delete -force $sdk_dir
 
-set hwproject "hw"
 set plat   ../../../fpga/implement/results/top.xsa
-set bsp "bsp"
 #set proc "ps7_cortexa9_0"
 #set proc "psu_cortexr5_0"
 set proc "psu_cortexa53_0"
@@ -20,9 +18,8 @@ platform generate
 domain create -name "streamer_domain" -os $os -proc $proc
 
 app create -name test2 -platform streamer -domain streamer_domain -template "Empty Application"
-app create -name test3 -platform streamer -domain streamer_domain -template "Hello World"
 
 file link -symbolic $sdk_dir/test2/src/main.c ../../../src/main.c                                                                                                                                            
 
-#app build -all
+app build test2
 
