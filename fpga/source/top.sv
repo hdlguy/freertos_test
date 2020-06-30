bfdd86d4244c38520f1306782eceea2352e75bb0
pedro <hdlguy@hotmail.com>
2020-06-30 16:23:33 -0400

    converted to zedboard to avoid fan noise

--- fpga/source/top.sv
+++ fpga/source/top.sv
@@ -1,37 +1,53 @@
 //
-module top ();
+module top (
+    inout   logic [14:0]    DDR_addr,
+    inout   logic [2:0]     DDR_ba,
+    inout   logic           DDR_cas_n,
+    inout   logic           DDR_ck_n,
+    inout   logic           DDR_ck_p,
+    inout   logic           DDR_cke,
+    inout   logic           DDR_cs_n,
+    inout   logic [3:0]     DDR_dm,
+    inout   logic [31:0]    DDR_dq,
+    inout   logic [3:0]     DDR_dqs_n,
+    inout   logic [3:0]     DDR_dqs_p,
+    inout   logic           DDR_odt,
+    inout   logic           DDR_ras_n,
+    inout   logic           DDR_reset_n,
+    inout   logic           DDR_we_n,
+    inout   logic           FIXED_IO_ddr_vrn,
+    inout   logic           FIXED_IO_ddr_vrp,
+    inout   logic [53:0]    FIXED_IO_mio,
+    inout   logic           FIXED_IO_ps_clk,
+    inout   logic           FIXED_IO_ps_porb,
+    inout   logic           FIXED_IO_ps_srstb,
+    //
+    output  logic [7:0]     led_tri_o
+);
 
-    logic[31:0] S_AXIS_S2MM_0_tdata;
-    logic[3:0]  S_AXIS_S2MM_0_tkeep;
-    logic       S_AXIS_S2MM_0_tlast;
-    logic       S_AXIS_S2MM_0_tready;
-    logic       S_AXIS_S2MM_0_tvalid;
-    logic       axi_aclk;
-    logic[0:0]  axi_areset;
-
-    system system_i(
-        .S_AXIS_S2MM_0_tdata    (S_AXIS_S2MM_0_tdata),
-        .S_AXIS_S2MM_0_tkeep    (S_AXIS_S2MM_0_tkeep),
-        .S_AXIS_S2MM_0_tlast    (S_AXIS_S2MM_0_tlast),
-        .S_AXIS_S2MM_0_tready   (S_AXIS_S2MM_0_tready),
-        .S_AXIS_S2MM_0_tvalid   (S_AXIS_S2MM_0_tvalid),
-        .axi_aclk               (axi_aclk),
-        .axi_aresetn            (axi_aresetn)
+    system system_i (
+        .DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .led_tri_o(led_tri_o)
     );
-    
-    assign S_AXIS_S2MM_0_tvalid = 1;
-    assign S_AXIS_S2MM_0_tkeep  = 4'hf;
-    assign S_AXIS_S2MM_0_tdata  = count;
-    logic[31:0] count;
-    always_ff @(posedge axi_aclk) begin
-        if (0 == axi_aresetn) begin
-            count <= 0;
-        end else begin
-            if (1 == S_AXIS_S2MM_0_tready) begin
-                count <= count + 1;
-            end
-        end       
-    end    
-    always_comb if (1 == (&count[3:0])) S_AXIS_S2MM_0_tlast = 1; else S_AXIS_S2MM_0_tlast = 0;
-            
+
 endmodule
