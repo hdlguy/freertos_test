aa5afd7cbc9ebf19cdb675360968231a9f5861d9
pedro <hdlguy@hotmail.com>
2020-07-07 07:59:02 -0400

    cleaned up bare metal interrupt example test2.c

--- software/vitis/intc_test/src/test2.c
+++ software/vitis/intc_test/src/test2.c
@@ -4,26 +4,23 @@
 #include "xil_exception.h"
 #include "xil_printf.h"
 
-#define INTC_DEVICE_ID		  XPAR_INTC_0_DEVICE_ID  // i think this is the ID of the INTC core.
-#define INTC_DEVICE_INT_ID	  XPAR_AXI_INTC_0_SYSTEM_PPS_INTR // i think this is the ID of the device wired into the INTC core.
-
 int IntcExample(u16 DeviceId);
-int SetUpInterruptSystem(XIntc *XIntcInstancePtr);
+int SetUpInterruptSystem(XIntc *XIntcInstancePtr, u16 DeviceId, u8 pps_device_id);
 void DeviceDriverHandler(void *CallbackRef);
 
 static XIntc InterruptController; 
 volatile static int InterruptProcessed = FALSE;
-static uint32_t led_vec;
 
 int main(void)
 {
 	int Status;
 	
-	led_vec = 0xffffff;
-	*((uint32_t*)XPAR_AXI_GPIO_0_BASEADDR) = 0x00ff & (led_vec >> 0);
-	*((uint32_t*)XPAR_AXI_GPIO_1_BASEADDR) = 0xffff & (led_vec >> 8);
+	(*((uint32_t*)XPAR_AXI_GPIO_1_BASEADDR)) = 0xffff; // set the PMOD LEDs
 	
-	Status = IntcExample(INTC_DEVICE_ID);
+	u16 DeviceId 		= XPAR_INTC_0_DEVICE_ID; 			// i think this is the ID of the INTC core.
+	u8  pps_device_id 	= XPAR_AXI_INTC_0_SYSTEM_PPS_INTR;	// i think this is the ID of the device wired into the INTC core.
+	Status = SetUpInterruptSystem(&InterruptController, DeviceId, pps_device_id);
+
 	if (Status != XST_SUCCESS) {
 		xil_printf("Intc Example Failed\r\n");
 		return XST_FAILURE;
@@ -34,34 +31,20 @@ int main(void)
 }
 
 
-int IntcExample(u16 DeviceId)
+int SetUpInterruptSystem(XIntc *XIntcInstancePtr, u16 DeviceId, u8 pps_device_id)
 {
 	int Status;
 
 	Status = XIntc_Initialize(&InterruptController, DeviceId);
 	if (Status != XST_SUCCESS) return XST_FAILURE;
 
-	Status = SetUpInterruptSystem(&InterruptController);
-	if (Status != XST_SUCCESS) return XST_FAILURE;
-	
-	while (1) {
-		if (InterruptProcessed) break;
-	}
-	return XST_SUCCESS;
-}
-
-
-int SetUpInterruptSystem(XIntc *XIntcInstancePtr)
-{
-	int Status;
-
-	Status = XIntc_Connect(XIntcInstancePtr, INTC_DEVICE_INT_ID, (XInterruptHandler)DeviceDriverHandler, (void *)0);
+	Status = XIntc_Connect(XIntcInstancePtr, pps_device_id, (XInterruptHandler)DeviceDriverHandler, (void *)0);
 	if (Status != XST_SUCCESS) return XST_FAILURE;
 
 	Status = XIntc_Start(XIntcInstancePtr, XIN_REAL_MODE); // XIN_SIMULATION_MODE);
 	if (Status != XST_SUCCESS) return XST_FAILURE;
 
-	XIntc_Enable(XIntcInstancePtr, INTC_DEVICE_INT_ID);
+	XIntc_Enable(XIntcInstancePtr, pps_device_id);
 
 	Xil_ExceptionInit();
 
@@ -76,7 +59,6 @@ int SetUpInterruptSystem(XIntc *XIntcInstancePtr)
 void DeviceDriverHandler(void *CallbackRef)
 {
 	InterruptProcessed = TRUE;
-	led_vec++;
-	*((uint32_t*)XPAR_AXI_GPIO_0_BASEADDR) = 0x00ff & (led_vec >> 0);
-	*((uint32_t*)XPAR_AXI_GPIO_1_BASEADDR) = 0xffff & (led_vec >> 8);
+	(*((uint32_t*)XPAR_AXI_GPIO_1_BASEADDR))--;  // decrement the PMOD LEDs
 }
+
