39ef277ee44c1d31cdc5681cd520940196ee86f2
pedro <hdlguy@hotmail.com>
2020-07-07 13:06:40 -0400

    added INTC interrupt setup to freertos program but cannot get INTC interrupts

--- software/vitis/freertos_test/src/freertos_hello_world.c
+++ software/vitis/freertos_test/src/freertos_hello_world.c
@@ -6,6 +6,8 @@
 /* Xilinx includes. */
 #include "xil_printf.h"
 #include "xparameters.h"
+#include "xintc.h"
+#include "xil_exception.h"
 
 #define TIMER_ID	1
 #define DELAY_15_SECONDS	15000UL
@@ -25,6 +27,12 @@ static QueueHandle_t xQueue = NULL;
 static TimerHandle_t xTimer = NULL;
 char HWstring[15] = "Hello!";
 long RxtaskCntr = 0;
+
+int SetUpInterruptSystem(XIntc *XIntcInstancePtr, u16 DeviceId, u8 pps_device_id);
+void DeviceDriverHandler(void *CallbackRef);
+
+static XIntc InterruptController; 
+volatile static int InterruptProcessed = FALSE;
 
 int main( void )
 {
@@ -47,6 +55,14 @@ int main( void )
 	configASSERT( xTimer );
 
 	xTimerStart( xTimer, 0 );
+
+
+
+//	u16 DeviceId 		= XPAR_INTC_0_DEVICE_ID; 			// i think this is the ID of the INTC core.
+//	u8  pps_device_id 	= XPAR_AXI_INTC_0_SYSTEM_PPS_INTR;	// i think this is the ID of the device wired into the INTC core.
+//	int Status = SetUpInterruptSystem(&InterruptController, DeviceId, pps_device_id);
+//	if (Status != XST_SUCCESS) xil_printf("SetUpInterruptSystem: FAILURE\n\r");
+
 
 	vTaskStartScheduler(); // Start the tasks and timer running.
 
@@ -75,7 +91,7 @@ static void prvRxTask( void *pvParameters )
 	for( ;; ) {
 		xQueueReceive( 	xQueue, Recdstring,	portMAX_DELAY );
 		xil_printf( "Rx task received string from Tx task: %s\r\n", Recdstring );
-		(*((uint32_t*)XPAR_AXI_GPIO_0_BASEADDR))++;
+		(*((uint32_t*)XPAR_AXI_GPIO_0_BASEADDR))--;
 		RxtaskCntr++;
 	}
 }
@@ -101,5 +117,36 @@ static void vTimerCallback( TimerHandle_t pxTimer )
 
 	vTaskDelete( xRxTask );
 	vTaskDelete( xTxTask );
+}
+
+int SetUpInterruptSystem(XIntc *XIntcInstancePtr, u16 DeviceId, u8 pps_device_id)
+{
+	int Status;
+
+	Status = XIntc_Initialize(&InterruptController, DeviceId);
+	if (Status != XST_SUCCESS) return XST_FAILURE;
+
+	Status = XIntc_Connect(XIntcInstancePtr, pps_device_id, (XInterruptHandler)DeviceDriverHandler, (void *)0);
+	if (Status != XST_SUCCESS) return XST_FAILURE;
+
+	Status = XIntc_Start(XIntcInstancePtr, XIN_REAL_MODE); // XIN_SIMULATION_MODE);
+	if (Status != XST_SUCCESS) return XST_FAILURE;
+
+	XIntc_Enable(XIntcInstancePtr, pps_device_id);
+
+	Xil_ExceptionInit();
+
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XIntc_InterruptHandler, XIntcInstancePtr);
+
+	Xil_ExceptionEnable();
+
+	return XST_SUCCESS;
+}
+
+
+void DeviceDriverHandler(void *CallbackRef)
+{
+	InterruptProcessed = TRUE;
+	(*((uint32_t*)XPAR_AXI_GPIO_1_BASEADDR))--;  // decrement the PMOD LEDs
 }
 
