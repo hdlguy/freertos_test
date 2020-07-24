365f76eb09097a9aa48158af598bdfee3d14fd69
pedro <hdlguy@hotmail.com>
2020-07-24 13:34:59 -0400

    created software project to use xdma under freertos

---
+++ uzed_xdma/software/vitis/xdma_test/setup.tcl
@@ -0,0 +1,19 @@
+# run at linux command line "xsct setup.tcl"
+file delete -force ./workspace
+
+set hw   ../../../fpga/implement/results/top.xsa
+#set proc "ps7_cortexa9_0"
+#set proc "psu_cortexr5_0"
+set proc "psu_cortexa53_0"
+
+setws ./workspace
+
+platform create -name "rtos_plat"    -hw $hw -proc $proc -os freertos10_xilinx
+domain create   -name "rtos_domain"          -proc $proc -os freertos10_xilinx 
+
+app create -name xdma_test -platform rtos_plat -domain rtos_domain -template "Empty Application"
+file link -symbolic ./workspace/xdma_test/src/xdma_test.c ../../../src/xdma_test.c
+
+
+
+
