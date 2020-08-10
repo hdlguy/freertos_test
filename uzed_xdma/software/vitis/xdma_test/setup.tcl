6d10492e82e0c1ba2a1e610c0604119166b19702
pedro <hdlguy@hotmail.com>
2020-08-10 16:27:51 -0400

    changed processor from A53 to R5 and now axidma works

--- uzed_xdma/software/vitis/xdma_test/setup.tcl
+++ uzed_xdma/software/vitis/xdma_test/setup.tcl
@@ -3,8 +3,8 @@ file delete -force ./workspace
 
 set hw   ../../../fpga/implement/results/top.xsa
 #set proc "ps7_cortexa9_0"
-#set proc "psu_cortexr5_0"
-set proc "psu_cortexa53_0"
+set proc "psu_cortexr5_0"
+#set proc "psu_cortexa53_0"
 
 setws ./workspace
 
