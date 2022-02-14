f53fd7fbff7405527e83d68ee5d94b4402e70d64
pete <peter.dudley@precisionplanting.com>
2022-02-14 11:52:52 -0500

    cleaned up flashboard.sh

--- microblaze/software/vitis/flashboard.sh
+++ microblaze/software/vitis/flashboard.sh
@@ -1,6 +1,8 @@
 updatemem -force -meminfo ../../implement/results/top.mmi  -bit ../../implement/results/top.bit -data ./workspace/freertos_test/Debug/freertos_test.elf -proc system_i/microblaze_0 -out ./download.bit 
 
-bootgen -arch fpga -image ./bootimage.bif -w -o ./BOOT.bin -interface spi 
+echo "the_ROM_image: { ./download.bit }" >> bootgen754.bif
+
+bootgen -arch fpga -image ./bootgen754.bif -w -o ./BOOT.bin -interface spi 
 
 program_flash -f ./BOOT.bin -offset 0 -flash_type mt25ql128-spi-x1_x2_x4 -cable type xilinx_tcf url TCP:127.0.0.1:3121 
 
